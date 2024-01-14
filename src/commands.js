ae154bfb0f55ae4172651a1ea923345fc90a8721
majkeloess <msaturczak@student.agh.edu.pl>
2024-01-14 01:23:20 +0100

    v1.2.0

--- src/commands.js
+++ src/commands.js
@@ -28,12 +28,20 @@ yargs(hideBin(process.argv))
             const mood = await newMood('bad', argv.description);
             console.log(`Bad mood added! Don't worry everything gonna be alright soon!!! Mood id: ${mood.id}`);
       })
+      .command('remove <id>', 'remove mood raport by id', yargs => {
+            return yargs.positional('id', {
+                  type: "number",
+            })
+      }, async argv => {
+            const id = removeMood(argv.id);
+      })
       .command('erase', 'delete your diary', () => { }, async argv => {
             await erase();
             console.log("Erased your mood diary!");
       })
       .command('diary', 'opens your mood diary', () => { }, async argv => {
-            startServer('items');
+            const moodList = await allMoods();
+            await startServer();
       })
       .demandCommand(1)
       .parse()
--- src/server.js
+++ src/server.js
@@ -2,18 +2,18 @@ import fs from "node:fs/promises"
 import http from "node:http"
 import open from "open"
 
-function createServer(items) {
-      return http.createServer((req, res) => {
+async function createServer() {
+      return http.createServer(async (req, res) => {
             const HTML_PATH = new URL('../index.html', import.meta.url).pathname;
-            const template = fs.readFile(HTML_PATH, 'utf-8');
+            const template = await fs.readFile(HTML_PATH, 'utf-8');
 
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            // res.end(items);
+            res.end(template);
       });
 }
 
-export function startServer(items) {
-      const server = createServer(items);
+export async function startServer() {
+      const server = await createServer();
       server.listen(2137, () => {
             console.log("Diary is waiting for you on: ", "\x1b[31mhttp://localhost:2137\x1b[0m");
       });
